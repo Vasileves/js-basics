2d9195c5748287aa43c5c461b95bb33a91d90232
Vasileves <55274720+Vasileves@users.noreply.github.com>
2020-01-31 13:37:02 +0300

    fix

--- 03/game.js
+++ 03/game.js
@@ -1,13 +1,24 @@
+function randomInteger(min, max) {
+  // получить случайное число от (min-0.5) до (max+0.5)
+  let rand = min - 0.5 + Math.random() * (max - min + 1);
+  return Math.round(rand);
+}
+
 // Координаты палуб
-let location1 = 5
-let location2 = 6
-let location3 = 7
+let location1 = randomInteger(0,9)
+let location2 = randomInteger(0,9)
+let location3 = randomInteger(0,9)
 //Кол-во выстрелов
 let currentShot = 0
 let shots = 0
 let hits = 0
 let isSunk = false
+let hit1 = false
+let hit2 = false
+let hit3 = false
 
+//Debug mode
+alert('ответы: ' + location1 + ', ' + location2 + ', ' + location3)
 //Цикл игры - работает, пока корабль не потонул
 while (isSunk === false) {
   // Получаем координаты выстрела
@@ -17,18 +28,25 @@ while (isSunk === false) {
   // Проверяем, попал или не попал
 
   if (currentShot === location1) {
-    hits += 1
+    hit1 = true
+    alert('В ячейке ' + currentShot + ' оказалась часть корабля!')
   }
 
   if (currentShot === location2) {
-    hits += 1
+    hit2 = true
+    alert('В ячейке ' + currentShot + ' оказалась часть корабля!')
   }
 
   if (currentShot === location3) {
-    hits += 1
+    hit3 = true
+    alert('В ячейке ' + currentShot + ' оказалась часть корабля!')
   }
 
-  if (hits > 2) {
+  if (currentShot < 0 || currentShot > 9) {
+    alert('Введённое число !')
+  }
+
+  if (hit1 === true && hit2 === true && hit3 === true) {
     isSunk = true
     alert("Корабль сбит, вы победили!")
   }
